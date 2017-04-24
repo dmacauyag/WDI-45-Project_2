d8dbcc9bb39f0a6d9d8d7c43d81fe0c981d0becc
Daniel Macauyag <dmacauyag92@gmail.com>
2017-04-23 18:27:25 -0700

    created Video model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170423181810) do
+ActiveRecord::Schema.define(version: 20170424012440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,15 @@ ActiveRecord::Schema.define(version: 20170423181810) do
     t.datetime "updated_at",      null: false
   end
 
+  create_table "videos", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "source"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_videos_on_user_id", using: :btree
+  end
+
+  add_foreign_key "videos", "users"
 end

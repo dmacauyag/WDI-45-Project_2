68728698008192e282152b7d8c9f31af9bf59e80
Daniel Macauyag <dmacauyag92@gmail.com>
2017-04-24 11:57:30 -0700

    updated video controller actions

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,9 +6,22 @@ class VideosController < ApplicationController
   end
 
   def new
+    @video = Video.new
   end
 
   def create
+    # new video object with info from the form
+    @video = Video.new(video_params)
+
+    # make this post belong to the current user that's submitting the form
+    @video.user = current_user
+
+    # then try to save it
+    if @video.save
+      redirect_to user_path(@user)
+    else
+      redirect_to new_post_path
+    end
   end
 
   def edit
@@ -18,5 +31,10 @@ class VideosController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def video_params
+    params.require(:video).permit(:title, :description, :source)
   end
 end

31fda67bc84a4c438407ff4ac6bd6acc97c5dbca
Daniel Macauyag <dmacauyag92@gmail.com>
2017-04-24 11:29:06 -0700

    fixed bug, new users can now be created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_sessions_path
+      redirect_to new_session_path
     end
   end
 

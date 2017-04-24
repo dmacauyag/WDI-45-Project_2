876e34a96c7079688941121dfdd2885e257655eb
Daniel Macauyag <dmacauyag92@gmail.com>
2017-04-24 11:42:46 -0700

    new users are redirected to personal page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
       redirect_to new_session_path
     end
   end

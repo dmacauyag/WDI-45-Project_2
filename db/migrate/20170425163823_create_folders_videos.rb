0f3a24e3eed2dbe36ae7261bf0431232327f04bf
Daniel Macauyag <dmacauyag92@gmail.com>
2017-04-25 10:03:02 -0700

    created joint folders videos table

--- db/migrate/20170425163823_create_folders_videos.rb
+++ db/migrate/20170425163823_create_folders_videos.rb
@@ -1,6 +1,8 @@
 class CreateFoldersVideos < ActiveRecord::Migration[5.0]
   def change
     create_table :folders_videos do |t|
+      t.belongs_to :folder, index: true
+      t.belongs_to :video, index: true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425163410) do
+ActiveRecord::Schema.define(version: 20170425163823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20170425163410) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_folders_on_user_id", using: :btree
+  end
+
+  create_table "folders_videos", force: :cascade do |t|
+    t.integer "folder_id"
+    t.integer "video_id"
+    t.index ["folder_id"], name: "index_folders_videos_on_folder_id", using: :btree
+    t.index ["video_id"], name: "index_folders_videos_on_video_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|

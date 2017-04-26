0efe8b9f5208d7ec266c432b1f4066ebdb7f8f0f
Daniel Macauyag <dmacauyag92@gmail.com>
2017-04-26 12:15:18 -0700

    moved video helper methods to video class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,26 +16,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # Checks the source of the video and applies the proper function to extract embed code
-  def video_source(str)
-    if str.include? 'www.youtube.com'
-      return youtube_embed(str)
-    elsif str.include? 'youtu.be'
-      return youtube_embed(str)
-    else
-      return nil
-    end
-  end
-
-  def youtube_embed(youtube_url)
-    if youtube_url[/youtu\.be\/([^\?]*)/]
-      youtube_id = $1
-    else
-      # Regex from # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
-      youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
-      youtube_id = $5
-    end
-    return "http://www.youtube.com/embed/#{ youtube_id }"
-  end
-
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,29 @@
 class Video < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :folders 
+  has_and_belongs_to_many :folders
+
+  # Checks the source of the video and applies the proper function to extract embed code
+  def video_source(str)
+    if str.include? 'www.youtube.com'
+      return youtube_embed(str)
+    elsif str.include? 'youtu.be'
+      return youtube_embed(str)
+    else
+      return nil
+    end
+  end
+
+  def youtube_embed(youtube_url)
+    if youtube_url[/youtu\.be\/([^\?]*)/]
+      youtube_id = $1
+    else
+      # Regex from # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
+      youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+      youtube_id = $5
+    end
+    return "http://www.youtube.com/embed/#{ youtube_id }"
+  end
+
+  before_save { |video| video.source = video_source(video.source) }
+  
 end

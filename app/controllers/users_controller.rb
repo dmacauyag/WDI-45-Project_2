1007555110db76fb730687a1d7cb2385b17dcd22
Daniel Macauyag <dmacauyag92@gmail.com>
2017-04-27 10:33:32 -0700

    cannot view another users edit profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    if current_user.id == params[:id].to_i
+      @user = User.find(params[:id])
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
